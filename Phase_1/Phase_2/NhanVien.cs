8d7bfe92c1af22543816344e6f41c92ab3685d50
dthgiang <duongthanhgiang.0108@gmail.com>
2023-05-06 02:42:31 +0700

    add nhanvien usercontrols

---
+++ Phase_1/Phase_2/NhanVien.cs
@@ -0,0 +1,145 @@
+﻿using Phase_1.UserControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Phase_1.Phase_2
+{
+    public partial class NhanVien : Form
+    {
+        bool sidebarExpand;
+        bool profileCollapsed;
+
+        private void addUserControl(UserControl userControl)
+        {
+            userControl.Dock = DockStyle.Fill;
+            panelContainer.Controls.Clear();
+            panelContainer.Controls.Add(userControl);
+            userControl.BringToFront();
+        }
+
+        public NhanVien()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void btnGray_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void picAvatar_Click(object sender, EventArgs e)
+        {
+            ProfileTimer.Start();
+        }
+
+        private void btnViewProfile_Click(object sender, EventArgs e)
+        {
+            XemProfile ql = new XemProfile();
+            addUserControl(ql);
+        }
+
+        private void btnSignOut_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Login login = new Login();
+            login.Show();
+        }
+
+        private void menuButton_Click(object sender, EventArgs e)
+        {
+            sidebarTimer.Start();
+        }
+
+        private void sidebarTimer_Tick(object sender, EventArgs e)
+        {
+            if (sidebarExpand)
+            {
+                sidebar.Width -= 10;
+                if (sidebar.Width == sidebar.MinimumSize.Width)
+                {
+                    sidebarExpand = false;
+                    sidebarTimer.Stop();
+                }
+            }
+            else
+            {
+                sidebar.Width += 10;
+                if (sidebar.Width == sidebar.MaximumSize.Width)
+                {
+                    sidebarExpand = true;
+                    sidebarTimer.Stop();
+                }
+            }
+        }
+
+        private void ProfileTimer_Tick(object sender, EventArgs e)
+        {
+            if (profileCollapsed)
+            {
+                panelThongTin.Height += 10;
+                if (panelThongTin.Height == panelThongTin.MaximumSize.Height)
+                {
+                    profileCollapsed = false;
+                    ProfileTimer.Stop();
+                }
+            }
+            else
+            {
+                panelThongTin.Height -= 10;
+                if (panelThongTin.Height == panelThongTin.MinimumSize.Height)
+                {
+                    profileCollapsed = true;
+                    ProfileTimer.Stop();
+                }
+            }
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            NhanVien nhanvien = new NhanVien();
+            nhanvien.Show();
+        }
+
+        private void btnTask_Click(object sender, EventArgs e)
+        {
+            QLTrucTiep_Task nhanvien = new QLTrucTiep_Task();
+            addUserControl(nhanvien);
+        }
+
+        private void btnPhongban_Click(object sender, EventArgs e)
+        {
+            QLTrucTiep_DSPhongBan ql = new QLTrucTiep_DSPhongBan();
+            addUserControl(ql);
+        }
+
+        private void btnDeAn_Click(object sender, EventArgs e)
+        {
+            QLTrucTiep_DSDeAn ql = new QLTrucTiep_DSDeAn();
+            addUserControl(ql);
+        }
+    }
+}
--- Phase_1/Program.cs
+++ Phase_1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Phase_1.Phase_2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace Phase_1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new NhanVien());
         }
     }
 }
